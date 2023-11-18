1b8a69c3d7f8dff65ec5ef1d1218b29e1d8b8f86
Pablo Andersen <pablo.andersen@gmail.com>
2023-11-18 15:14:54 -0300

    terminamos la maqueta

--- TP4/js/main.js
+++ TP4/js/main.js
@@ -6,9 +6,9 @@ function stickyLogo() {
 
     // Obtener la posición actual de desplazamiento
     var scrollPosition = window.scrollY || document.documentElement.scrollTop;
-
+    console.log(scrollPosition);
     // Agregar la clase 'small' cuando el desplazamiento es mayor que 100px (puedes ajustar este valor)
-    if (scrollPosition > 50) {
+    if (scrollPosition > 200) {
         logo.classList.add("small");
     } else {
         logo.classList.remove("small");
