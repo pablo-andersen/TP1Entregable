dacc654c1f1339edcb3f71a17b629dd4c1970ff9
Pablo Andersen <pablo.andersen@gmail.com>
2023-11-23 20:59:26 -0300

    aplicamos sugerencias de Christian

--- TP4/js/main.js
+++ TP4/js/main.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
     // porcentaje de carga que se presenta al inicio
     let contenedor = document.querySelector(".loaderContainer");
     const telarania = document.querySelector(".telarania");
@@ -16,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
     function cargarSimulada() {
         if (avance < 100) {
             telarania.style.opacity = avance/100;
-            avance += 100;
+            avance += 1;
             posicion+= (360/100);     
             posicionLineaBlanca += (360/100);
             arania.style.top = posicion + "px";
@@ -50,9 +49,42 @@ let imgSticky01 =  document.querySelector("#imgSticky01");
 let imgSticky02 =  document.querySelector("#imgSticky02");
 let imgSticky03 =  document.querySelector("#imgSticky03");
 let imgSticky04 =  document.querySelector("#imgSticky04");
+let txtSticky01 =  document.querySelector("#textSticky01");
+let txtSticky02 =  document.querySelector("#textSticky02");
+let txtSticky03 =  document.querySelector("#textSticky03");
+let txtSticky04 =  document.querySelector("#textSticky04");
+let bannerGhost = document.querySelector('.bannerGhost');
 let demo01 = document.querySelector("#demo01");
 let demo02 = document.querySelector("#demo02");
 let demo03 = document.querySelector("#demo03");
+let sprite = document.querySelector(".spriteSheet");
+
+demo01.addEventListener("mouseover", (e)=> {
+    demo01.classList.remove('demoOut');
+    demo01.classList.add('demoIn');
+});
+demo01.addEventListener("mouseout", (e)=>{
+    demo01.classList.remove('demoIn');
+    demo01.classList.add('demoOut');
+});
+
+demo02.addEventListener("mouseover", (e)=> {
+    demo02.classList.remove('demoOut');
+    demo02.classList.add('demoIn');
+});
+demo02.addEventListener("mouseout", (e)=>{
+    demo02.classList.remove('demoIn');
+    demo02.classList.add('demoOut');
+});
+
+demo03.addEventListener("mouseover", (e)=> {
+    demo03.classList.remove('demoOut');
+    demo03.classList.add('demoIn');
+});
+demo03.addEventListener("mouseout", (e)=>{
+    demo03.classList.remove('demoIn');
+    demo03.classList.add('demoOut');
+});
 
 document.addEventListener("scroll", (e)=>{
     
@@ -78,25 +110,43 @@ document.addEventListener("scroll", (e)=>{
         flexCard02.classList.remove("showUp");
         flexCard03.classList.remove("showUp");
     }
-    if(window.scrollY >= 5363){
-        imgSticky04.style.opacity = 0;
+
+
+    if (window.scrollY < 3878) {
+        imgSticky01.style.opacity = 0;
+        txtSticky01.style.opacity = 0;
     }
-    else if (window.scrollY >= 5063 && window.scrollY < 5363){
-        imgSticky03.style.opacity = 0;
-        imgSticky04.style.opacity = 1;
-    } else  if (window.scrollY >= 4563 && window.scrollY < 5063){
-        imgSticky02.style.opacity = 0;
-        imgSticky03.style.opacity = 1;
-        imgSticky04.style.opacity = 0;
-    } else  if (window.scrollY >= 4063 && window.scrollY < 4563){
-        imgSticky01.style.opacity = 0;
-        imgSticky02.style.opacity = 1;
-        imgSticky03.style.opacity = 0;
-    } else  if (window.scrollY >= 3763 && window.scrollY < 4063){
-        imgSticky02.style.opacity = 0;
+    else if (window.scrollY > 3700 && window.scrollY < 4200) {
         imgSticky01.style.opacity = 1;
-    } else {
+        txtSticky01.style.opacity = 1;
+        imgSticky02.style.opacity = 0;
+        txtSticky02.style.opacity = 0;
+    }
+    else if (window.scrollY > 4200 && window.scrollY < 4700) {
         imgSticky01.style.opacity = 0;
+        txtSticky01.style.opacity = 0;
+        imgSticky02.style.opacity = 1;
+        txtSticky02.style.opacity = 1;
+        imgSticky03.style.opacity = 0;
+        txtSticky03.style.opacity = 0;
+    }
+    else if (window.scrollY > 4700 && window.scrollY < 5400) {
+        imgSticky02.style.opacity = 0;
+        txtSticky02.style.opacity = 0;
+        imgSticky03.style.opacity = 1;
+        txtSticky03.style.opacity = 1;
+        imgSticky04.style.opacity = 0;
+        txtSticky04.style.opacity = 0;
+    }
+    else if (window.scrollY > 5400 && window.scrollY < 6000) {
+        imgSticky03.style.opacity = 0;
+        txtSticky03.style.opacity = 0;
+        imgSticky04.style.opacity = 1;
+        txtSticky04.style.opacity = 1;
+    } 
+    else {
+        imgSticky04.style.opacity = 0;
+        txtSticky04.style.opacity = 0;
     }
 });
 
@@ -105,7 +155,6 @@ window.addEventListener("scroll", (e)=>{
     let top = window.scrollY;
     let speed, yPos;
     let currentScrollPos = window.scrollY;
-    let stepScroll = currentScrollPos-scrollPos;
     let scale = 1;
 
     // mover el cielo
@@ -127,6 +176,8 @@ window.addEventListener("scroll", (e)=>{
     speed = edificioFdo.getAttribute('data-speed');
     yPos = (top * (speed / 40));
     edificioFdo.setAttribute('style', 'transform: translate3d( 0px, ' + yPos + 'px, 0px)');
+
+
     if (window.scrollY < 300){
         
         yPos = mainSpideyBlanco.getAttribute('top')-top;
@@ -147,34 +198,20 @@ window.addEventListener("scroll", (e)=>{
         telaDer.setAttribute('style', 'transform: translate3d( '+ -yPos/4 +'px, ' + yPos + 'px, 0px)');
     }
 
-    if(top >= 200 && top < 1400){
+    if(top >= 100 && top < 750){
         let pos = duende.getBoundingClientRect(); 
         if (pos.top < window.innerHeight && pos.bottom > 0) {
           duende.style.transform = 'translateY('+
-            (window.scrollY - pos.top) * 0.0275
+            (window.scrollY - pos.top) * 0.25
           +'px)';
         }
     }
+    
+    if (top > 1200 && top <= 2858){
+        let pos = bannerGhost.getBoundingClientRect(); 
+        bannerGhost.style.transform = 'translateY('+window.scrollY * 0.2+'px)';
+    }
+    
     scrollPos = currentScrollPos;
 
-    if (top > 1525 && top <= 2774){
-        let pos = demo01.getBoundingClientRect(); 
-        if (pos.top < window.innerHeight && pos.bottom > 0) {
-          demo01.style.transform = 'translateY('+
-            (window.scrollY - pos.top) * 0.02
-          +'px)';
-        }
-        pos = demo02.getBoundingClientRect(); 
-        if (pos.top < window.innerHeight && pos.bottom > 0) {
-          demo02.style.transform = 'translateY('+
-            (window.scrollY - pos.top) * 0.026
-          +'px)';
-        }
-        pos = demo03.getBoundingClientRect(); 
-        if (pos.top < window.innerHeight && pos.bottom > 0) {
-          demo03.style.transform = 'translateY('+
-            (window.scrollY - pos.top) * 0.033
-          +'px)';
-        }
-    }
 });
